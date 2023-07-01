ee6f07048e050b4b40079d46a6336b47cf22b97e
stnDinus <111202214433@mhs.dinus.ac.id>
2023-07-01 12:32:08 +0700

    feat(tutorial): langkah 7, lsp format

--- app/Controllers/Tweet.php
+++ app/Controllers/Tweet.php
@@ -6,72 +6,92 @@ use App\Models\TweetModel;
 
 class Tweet extends BaseController
 {
-    public $categories;
-    public $sess;
-    public $curUser;
+  public $categories;
+  public $sess;
+  public $curUser;
 
-    public $tweetMdl;
-    public $profile;
+  public $tweetMdl;
+  public $profile;
 
-    public function __construct()
-    {
-        $this->categories = (new \Config\AdtConfig())->getCategories();
-        $this->sess = session();
-        $this->curUser = $this->sess->get('currentuser');
+  public function __construct()
+  {
+    $this->categories = (new \Config\AdtConfig())->getCategories();
+    $this->sess = session();
+    $this->curUser = $this->sess->get('currentuser');
 
-        $this->tweetMdl = new TweetModel();
-        $userMdl = new \App\Models\UserModel();
-        $this->profile = $userMdl->find($this->curUser['userid']);
+    $this->tweetMdl = new TweetModel();
+    $userMdl = new \App\Models\UserModel();
+    $this->profile = $userMdl->find($this->curUser['userid']);
+  }
+
+  public function index()
+  {
+    $data['categories'] = $this->categories;
+    $data['judul'] = 'Tweet Terbaru';
+
+    $data['profile'] = $this->profile;
+    $data['tweets'] = $this->tweetMdl->getLatest();
+
+    return view('tweet_home', $data);
+  }
+
+  public function category($category)
+  {
+    $data['categories'] = $this->categories;
+    $data['judul'] = 'Tweet Terbaru';
+
+    $data['profile'] = $this->profile;
+    $data['tweets'] = $this->tweetMdl->getByCategory($category);
+
+    return view('tweet_home', $data);
+  }
+
+  public function addForm()
+  {
+    $data['categories'] = $this->categories;
+    return view('tweet_add', $data);
+  }
+
+  public function editForm($tweet_id)
+  {
+    $tweet = $this->tweetMdl->find($tweet_id);
+    if ($tweet->user_id != $this->sess->get('currentuser')['userid']) {
+      $this->sess->set('edittweet', 'error');
+      return redirect()->to('/');
     }
 
-    public function index()
-    {
-        $data['categories'] = $this->categories;
-        $data['judul'] = 'Tweet Terbaru';
+    $data['categories'] = $this->categories;
+    $data['tweet'] = $tweet;
+    return view('tweet_edit', $data);
+  }
 
-        $data['profile'] = $this->profile;
-        $data['tweets'] = $this->tweetMdl->getLatest();
-
-        return view('tweet_home', $data);
+  public function editTweet()
+  {
+    $result = $this->tweetMdl->editTweet($this->request->getPost());
+    if ($result) {
+      $this->sess->setFlashdata('edittweet', 'success');
+    } else {
+      $this->sess->setFlashdata('edittweet', 'error');
     }
 
-    public function category($category)
-    {
-        $data['categories'] = $this->categories;
-        $data['judul'] = 'Tweet Terbaru';
+    return redirect()->to('/');
+  }
 
-        $data['profile'] = $this->profile;
-        $data['tweets'] = $this->tweetMdl->getByCategory($category);
+  public function addTweet()
+  {
+    $this->tweetMdl->newTweet($this->sess->get('currentuser'), $this->request->getPost());
+    $this->sess->setFlashdata('addtweet', 'success');
+    return redirect()->to('/');
+  }
 
-        return view('tweet_home', $data);
+  public function delTweet($tweet_id)
+  {
+    $result = $this->tweetMdl->delTweet($this->sess->get('currentuser')['userid'], $tweet_id);
+    if ($result) {
+      $this->sess->setFlashdata('deltweet', 'success');
+    } else {
+      $this->sess->setFlashdata('deltweet', 'error');
     }
-
-    public function addForm()
-    {
-        $data['categories'] = $this->categories;
-        return view('tweet_add', $data);
-    }
-
-    public function editForm($tweet_id)
-    {
-        $data['categories'] = $this->categories;
-        return view('tweet_edit', $data);
-    }
-
-    public function addTweet()
-    {
-        $this->tweetMdl->newTweet($this->sess->get('currentuser'), $this->request->getPost());
-        $this->sess->setFlashdata('addtweet', 'success');
-        return redirect()->to('/');
-    }
-
-    public function delTweet($tweet_id)
-    {
-        return redirect()->to('/');
-    }
-
-    public function editTweet()
-    {
-        return redirect()->to('/');
-    }
+    return redirect()->to('/');
+  }
 }
--- app/Models/TweetModel.php
+++ app/Models/TweetModel.php
@@ -7,40 +7,65 @@ use CodeIgniter\Model;
 
 class TweetModel extends Model
 {
-    protected $table = 'tweets';
-    protected $allowedFields = [
-        'user_id', 'content', 'category'
-    ];
+  protected $table = 'tweets';
+  protected $allowedFields = [
+    'user_id', 'content', 'category'
+  ];
 
-    protected $returnType = \App\Entities\Tweet::class;
-    public $rules = [
-        'content' => 'required',
-        'category' => 'required'
-    ];
+  protected $returnType = \App\Entities\Tweet::class;
+  public $rules = [
+    'content' => 'required',
+    'category' => 'required'
+  ];
 
-    public function newTweet($curUser, $post)
-    {
-        $tweet = new Tweet();
-        $tweet->user_id = $curUser['userid'];
-        $tweet->content = $post['content'];
-        $tweet->category = $post['category'];
-        $this->save($tweet);
+  public function newTweet($curUser, $post)
+  {
+    $tweet = new Tweet();
+    $tweet->user_id = $curUser['userid'];
+    $tweet->content = $post['content'];
+    $tweet->category = $post['category'];
+    $this->save($tweet);
+  }
+
+  public function getLatest()
+  {
+    $query = $this->select('tweets.id, user_id, username, fullname, content, category, created_at')
+      ->orderBy('created_at', 'desc')
+      ->join('users', 'users.id = tweets.user_id');
+    return $query->findAll();
+  }
+
+  public function getByCategory($category)
+  {
+    $query = $this->select('tweets.id, user_id, username, fullname, content, category, created_at')
+      ->where('category', $category)->orderBy('created_at', 'desc')
+      ->join('users', 'users.id = tweets.user_id');;
+    return $query->findAll();
+  }
+
+  public function editTweet($post)
+  {
+    $tweet = $this->find($post['id']);
+    if ($tweet) {
+      $tweet->content = $post['content'];
+      $tweet->category = $post['category'];
+      $this->save($tweet);
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public function getLatest()
-    {
-        $query = $this->select('tweets.id, user_id, username, fullname, content, category, created_at')
-                    ->orderBy('created_at', 'desc')
-                    ->join('users', 'users.id = tweets.user_id');
-        return $query->findAll();
+  public function delTweet($user_id, $tweet_id)
+  {
+    $tweet = $this->find($tweet_id);
+    if ($tweet) {
+      if ($user_id == $tweet->user_id) {
+        $this->delete($tweet->id, true);
+        return true;
+      } else {
+        return false;
+      }
     }
-
-    public function getByCategory($category)
-    {
-        $query = $this->select('tweets.id, user_id, username, fullname, content, category, created_at')
-                    ->where('category', $category)->orderBy('created_at', 'desc')
-                    ->join('users', 'users.id = tweets.user_id');
-                    ;
-        return $query->findAll();
-    }
+  }
 }
